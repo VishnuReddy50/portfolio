83f394caaa1cae1364644f6306754376c47f1c0b
Vishnu Vardhan Reddy <cvishnureddy5500@gmail.com>
2021-07-08 10:51:16 +0530

    myanother commit

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -9,8 +9,19 @@ function Nav() {
 	const work = "< Work />";
 	const contact = "< Contact />";
 
+	var prevScrollpos = window.pageYOffset;
+	window.onscroll = function () {
+		var currentScrollPos = window.pageYOffset;
+		if (prevScrollpos > currentScrollPos) {
+			document.getElementById("nav").style.top = "0";
+		} else {
+			document.getElementById("nav").style.top = "-50px";
+		}
+		prevScrollpos = currentScrollPos;
+	}
+
 	return (
-		<div className="nav">
+		<div className="nav" id="nav">
 			<div className="navTitle">
 				<a href="#landing">{name}</a>
 			</div>
